443c6900147acd8c2ee22266e1632bff1c8280a7
Davi Nakano <davinakanoca@gmail.com>
2023-09-06 14:07:46 -0700

    Add DefaultLayout and a minimal Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,12 +4,16 @@ import { Home } from "./pages/Home";
 import { Checkout } from "./pages/Checkout";
 import { OrderSuccess } from "./pages/OrderSuccess";
 
+import { DefaultLayout } from "./layouts/DefaultLayout";
+
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/order-success" element={<OrderSuccess />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-success" element={<OrderSuccess />} />
+      </Route>
     </Routes>
   );
 }
