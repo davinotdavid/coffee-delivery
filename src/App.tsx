6855995880e96576d810afb879b477422cf9256b
Davi Nakano <davinakanoca@gmail.com>
2023-09-06 13:52:33 -0700

    Configure global styles, theme colors and theme type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hey!</h1>
-    </>
-  )
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
