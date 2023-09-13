18d5537ad8233647016eb201924a33ee64fdf7e1
Davi Nakano <davinakanoca@gmail.com>
2023-09-13 10:41:53 -0700

    Add OrderSuccess page

--- src/pages/OrderSuccess/index.tsx
+++ src/pages/OrderSuccess/index.tsx
@@ -1,3 +1,70 @@
+import { useContext } from "react";
+import { CartContext } from "@/contexts/CartContext";
+import OrderCompletedSVG from "@/assets/orderCompleted.svg";
+import {
+  StyledMain,
+  Flex,
+  GradientBorderContainer,
+  InfoContainer,
+} from "./styles";
+import { IconContainer } from "@/components/IconContainer";
+import { CurrencyDollar, MapPin, Timer } from "phosphor-react";
+import { PAYMENT_METHOD_MAP } from "@/constants";
+
 export function OrderSuccess() {
-  return <h1>YO</h1>;
+  const { address, paymentMethod } = useContext(CartContext);
+
+  return (
+    <StyledMain>
+      <h1>Hooray! Order confirmed!</h1>
+      <p>Now it is just a little wait before the coffee reaches you!</p>
+
+      <Flex>
+        <GradientBorderContainer>
+          <InfoContainer>
+            <IconContainer icon="pin">
+              <MapPin size={16} weight="fill" />
+            </IconContainer>
+
+            <p>
+              Delivery address:{" "}
+              <strong>
+                {address?.street}
+                {address?.unit ? `, ${address.unit}` : ""}
+              </strong>
+              <br /> {address?.city} {address?.province}
+            </p>
+          </InfoContainer>
+
+          <InfoContainer>
+            <IconContainer icon="timer">
+              <Timer size={16} weight="fill" />
+            </IconContainer>
+
+            <p>
+              Delivery forecast:
+              <br /> <strong>20 min - 30 min</strong>
+            </p>
+          </InfoContainer>
+
+          <InfoContainer>
+            <IconContainer icon="dollar">
+              <CurrencyDollar size={16} weight="fill" />
+            </IconContainer>
+
+            <p>
+              Payment upon delivery
+              <br />{" "}
+              <strong>{PAYMENT_METHOD_MAP[paymentMethod || "cash"]}</strong>
+            </p>
+          </InfoContainer>
+        </GradientBorderContainer>
+
+        <img
+          src={OrderCompletedSVG}
+          alt="An illustration of a person riding a purple scooter with a box attached to it"
+        />
+      </Flex>
+    </StyledMain>
+  );
 }
--- src/pages/OrderSuccess/styles.ts
+++ src/pages/OrderSuccess/styles.ts
@@ -1,3 +1,61 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const StyledMain = styled.main`
+  max-width: 72rem;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+    font-family: "Baloo 2", cursive;
+    color: ${(props) => props.theme.colors.primaryDark};
+    margin-bottom: 0.25rem;
+  }
+
+  p {
+    margin-bottom: 2.5rem;
+  }
+`;
+
+export const Flex = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+`;
+
+export const GradientBorderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  position: relative;
+  background: ${(props) => props.theme.colors.white};
+  background-clip: padding-box;
+  border: 1px solid transparent;
+  border-radius: 6px 45px;
+  padding: 2.5rem;
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -1px;
+    border-radius: inherit;
+    background: linear-gradient(#dbac2c, #8047f8);
+  }
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  color: ${(props) => props.theme.colors.baseText};
+
+  p {
+    margin: 0;
+  }
+`;
